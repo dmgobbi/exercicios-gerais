364e5e1c4fb9ade288164f887323d0fc3647b58e
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-08-30 12:58:10 -0300

    Adicionar resolucao rec_05

---
+++ 02_recursao/rec_05/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fatorial(int n);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int resultado = fatorial(n);
+    printf("%d\n", resultado);
+    return 0;
+}
+
+int fatorial(int n) {
+    if (n == 1) {
+        return n;
+    }
+    return n * fatorial(n - 1);
+}
