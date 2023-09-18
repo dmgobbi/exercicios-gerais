9ac8f6aec40a28c53662f43b5892d9d091368735
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-18 16:24:11 +0000

    Refatora o código do tabuleiro para melhor clareza e consistência

--- 04_TAD_simples/TAD_01/Respostas/Daniel/tabuleiro.c
+++ 04_TAD_simples/TAD_01/Respostas/Daniel/tabuleiro.c
@@ -2,66 +2,74 @@
 #include <stdio.h>
 
 tTabuleiro CriaTabuleiro() {
-    tTabuleiro tabuleiro;
+  tTabuleiro tabuleiro;
+  tabuleiro.peca1 = 'X';
+  tabuleiro.peca2 = '0';
+  tabuleiro.pecaVazio = '-';
 
-    for (int i = 0; i < TAM_TABULEIRO; i++) {
-        for (int j = 0; j < TAM_TABULEIRO; j++) {
-            tabuleiro.posicoes[i][j] = '-';
-        }
+  for (int i = 0; i < TAM_TABULEIRO; i++) {
+    for (int j = 0; j < TAM_TABULEIRO; j++) {
+      tabuleiro.posicoes[i][j] = tabuleiro.pecaVazio;
     }
-
-    tabuleiro.peca1 = 'X';
-    tabuleiro.peca2 = '0';
-    tabuleiro.pecaVazio = '-';
-
-    return tabuleiro;
+  }
+  return tabuleiro;
 }
 
 tTabuleiro MarcaPosicaoTabuleiro(tTabuleiro tabuleiro, int peca, int x, int y) {
-    switch (peca) {
-        case 1:
-            tabuleiro.posicoes[x][y] = tabuleiro.peca1;
-            break;
-        case 2:
-            tabuleiro.posicoes[x][y] = tabuleiro.peca2;
-            break;
-        default: 
-            printf("ERRO: Peca invalida.\n");
-    }
-    return tabuleiro;
+  switch (peca) {
+  case 1:
+    tabuleiro.posicoes[y][x] = tabuleiro.peca1;
+    break;
+  case 2:
+    tabuleiro.posicoes[y][x] = tabuleiro.peca2;
+    break;
+  default:
+    printf("[ERRO] Jogador invalido!\n");
+  }
+  return tabuleiro;
 }
 
 int TemPosicaoLivreTabuleiro(tTabuleiro tabuleiro) {
-    for (int i = 0; i < TAM_TABULEIRO; i++) {
-        for (int j = 0; j < TAM_TABULEIRO; j++) {
-            tabuleiro.posicoes[i][j] == tabuleiro.pecaVazio;
-            return 1;
-        }
+  for (int i = 0; i < TAM_TABULEIRO; i++) {
+    for (int j = 0; j < TAM_TABULEIRO; j++) {
+      if (tabuleiro.posicoes[i][j] == tabuleiro.pecaVazio) {
+        return 1;
+      }
     }
+  }
+  return 0;
+}
+
+int EstaMarcadaPosicaoPecaTabuleiro(tTabuleiro tabuleiro, int x, int y,
+                                    int peca) {
+  switch (peca) {
+  case 1:
+    return tabuleiro.posicoes[y][x] == tabuleiro.peca1;
+  case 2:
+    return tabuleiro.posicoes[y][x] == tabuleiro.peca2;
+  default:
+    printf("[ERRO] Jogador Invalido!\n");
     return 0;
+  }
 }
 
-int EstaMarcadaPosicaoPecaTabuleiro(tTabuleiro tabuleiro, int x, int y, int peca) {
-    switch (peca) {
-        case 1: 
-            return tabuleiro.posicoes[x][y] == tabuleiro.peca1;
-        case 2:
-            return tabuleiro.posicoes[x][y] == tabuleiro.peca2;
-        default: 
-            printf("ERRO: Peca invalida.\n");
-            return -1;
-    }
+int EstaLivrePosicaoTabuleiro(tTabuleiro tabuleiro, int x, int y) {
+ return tabuleiro.posicoes[y][x] == tabuleiro.pecaVazio;
 }
 
-int EhPosicaoValidaTabuleiro(int x, int y) { 
-    return (x >= 0 && x < TAM_TABULEIRO && y >= 0 && y < TAM_TABULEIRO);
+int EhPosicaoValidaTabuleiro(int x, int y) {
+  return (x >= 0 && x < TAM_TABULEIRO && 
+          y >= 0 && y < TAM_TABULEIRO);
 }
 
 void ImprimeTabuleiro(tTabuleiro tabuleiro) {
-    for (int i = 0; i < TAM_TABULEIRO; i++) {
-        for (int j = 0; j < TAM_TABULEIRO; j++) {
-           printf("%c", tabuleiro.posicoes[i][j]);
-        }
-        printf("\n");
+  for (int i = 0; i < TAM_TABULEIRO; i++) {
+    for (int j = 0; j < TAM_TABULEIRO; j++) {
+      if (j == 0) {
+        printf("\t");
+      }
+      printf("%c", tabuleiro.posicoes[i][j]);
     }
+    printf("\n");
+  }
 }
