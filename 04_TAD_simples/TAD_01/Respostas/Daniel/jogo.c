79e78c2f983c6a1640aa291e52211da9afebec37
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-18 17:55:25 +0000

    Refatora o código do jogo para melhor legibilidade e funcionalidade

--- 04_TAD_simples/TAD_01/Respostas/Daniel/jogo.c
+++ 04_TAD_simples/TAD_01/Respostas/Daniel/jogo.c
@@ -1,28 +1,53 @@
 #include "jogo.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 tJogo CriaJogo() {
     tJogo jogo;
+    jogo.jogador1 = CriaJogador(ID_JOGADOR_1);
+    jogo.jogador2 = CriaJogador(ID_JOGADOR_2);
     jogo.tabuleiro = CriaTabuleiro();
-    jogo.jogador1 = CriaJogador(1);
-    jogo.jogador2 = CriaJogador(2);
     return jogo;
 }
 
-void ComecaJogo(tJogo jogo) {}
+void ComecaJogo(tJogo jogo) {
+  while (1) {
+    jogo.tabuleiro = JogaJogador(jogo.jogador1, jogo.tabuleiro);
+    ImprimeTabuleiro(jogo.tabuleiro);
+    if (VenceuJogador(jogo.jogador1, jogo.tabuleiro) || AcabouJogo(jogo)) {
+      printf("JOGADOR 1 Venceu!\n");
+      break;
+    }
+
+    jogo.tabuleiro = JogaJogador(jogo.jogador2, jogo.tabuleiro);
+    ImprimeTabuleiro(jogo.tabuleiro);
+    if (VenceuJogador(jogo.jogador2, jogo.tabuleiro) || AcabouJogo(jogo)) {
+      printf("JOGADOR 2 Venceu!\n");
+      break;
+    }    
+  }
+
+  if (!ContinuaJogo()) {
+    exit(1);
+  }
+}
 
 int AcabouJogo(tJogo jogo) {
     return !TemPosicaoLivreTabuleiro(jogo.tabuleiro);
 }
 
 int ContinuaJogo() {
-    char opcao;
+    char opcao = '\0';
     while (1) {
-        printf("Jogar novamente? (s,n) ");
-        scanf("%c", &opcao);
+      printf("Jogar novamente? (s,n)\n");
+
+      scanf("%*[^sn]");
+      if (scanf("%c", &opcao) == 1) {
         if (opcao == 's' || opcao == 'n') {
-            break;
+          break;
         } 
+      }
+      scanf("%*[^\n]");
     }
     return opcao == 's';
 }
