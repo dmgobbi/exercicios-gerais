f5b185ce3da4ee4cd0a4509ab15d9db46b0d9926
dmgobbi <daniel.gobbi@edu.ufes.com>
2023-09-21 23:23:21 -0300

    feat: Adicionar implementação da função TrocaInterios e concluir PONT_01

---
+++ 05_ponteiros/pont_01/Respostas/Daniel/utils.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "utils.h"
+
+int TrocaInterios(int *n1, int *n2) {
+    if (*n1 > *n2) {
+        int aux = *n1;
+        *n1 = *n2;
+        *n2 = aux;
+        return 1; 
+    } 
+    return 0;
+}
