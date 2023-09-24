86606456a3c3b7602004cb6476601d9afdacb5ee
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-24 11:22:21 -0300

    Adicionar implementação completa dos arquivos funcionario.c e main.c

---
+++ 04_TAD_simples/TAD_09/Respostas/Daniel/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "empresa.h"
+
+int main() {
+    int qtdEmpresas;
+    scanf("%d", &qtdEmpresas);
+    tEmpresa empresa[qtdEmpresas];
+
+    for (int i = 0; i < qtdEmpresas; i++) {
+        empresa[i] = leEmpresa();
+    }
+
+    for (int i = 0; i < qtdEmpresas; i++) {
+        imprimeEmpresa(empresa[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
