fff0ad840ccb43e313e370f57bd20a8b63d031ba
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-25 14:26:35 -0300

    Adicionar implementação completa de filme.c

---
+++ 04_TAD_simples/TAD_03/Respostas/Daniel/filme.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "filme.h"
+
+tFilme criarFilme (char* nome, int codigo, int valor, int quantidade) {
+    tFilme filme;
+    strcpy(filme.nome, nome);
+    filme.codigo = codigo;
+    filme.valor = valor;
+    filme.qtdEstoque = quantidade;
+    filme.qtdAlugada = 0;
+    return filme;
+}
+
+tFilme lerFilme () {
+    tFilme filme;
+    int codigo, valor, quantidade;
+    char nome[MAX_CARACTERES];
+
+    if (scanf("%d,%s,%d,%d", &codigo, nome, &valor, &quantidade) != 4) {
+        printf("Erro ao ler filme\n");
+        exit(1);
+    }
+
+    filme = criarFilme(nome, codigo, valor, quantidade);
+    return filme;
+}
+
+int obterCodigoFilme (tFilme filme) {
+    return filme.codigo;
+}
+
+void imprimirNomeFilme (tFilme filme) {
+    printf("%s", filme.nome);
+}
+
+int obterValorFilme (tFilme filme) {
+    return filme.valor;
+}
+
+int obterQtdEstoqueFilme (tFilme filme) {
+    return filme.qtdEstoque;
+}
+
+int obterQtdAlugadaFilme (tFilme filme) {
+    return filme.qtdAlugada;
+}
+
+int ehMesmoCodigoFilme (tFilme filme, int codigo) {
+    return filme.codigo == codigo;
+}
+
+tFilme alugarFilme (tFilme filme) {
+    filme.qtdAlugada++;
+    filme.qtdEstoque--;
+    return filme;
+}
+
+tFilme devolverFilme (tFilme filme) {
+    filme.qtdAlugada--;
+    filme.qtdEstoque++;
+    return filme;
+}
+
+int compararNomesFilmes (tFilme filme1, tFilme filme2) {
+    return strcmp(filme1.nome, filme2.nome);
+}
