18f5f8c5ee68940f6ac2c85d084f482e4731259f
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-24 23:16:56 -0300

    Pequena melhoria na legibilidade do código

--- 05_ponteiros/pont_03/Respostas/Daniel/vetor.c
+++ 05_ponteiros/pont_03/Respostas/Daniel/vetor.c
@@ -5,6 +5,7 @@
 void LeDadosParaVetor(int * vet, int tam) {
     for (int i = 0; i < tam; i++) {
         scanf("%d", &vet[i]);
+        // scanf("%d", vet + i);
     }
 }
 
@@ -19,7 +20,7 @@ void TrocaSeAcharMenor(int *vet, int tam, int *paraTrocar) {
     int menor = vet[*paraTrocar];
     int indexMenor = *paraTrocar;
 
-    for (int i = *paraTrocar + 1; i < tam; i++) {
+    for (int i = *paraTrocar; i < tam; i++) {
         if (vet[i] < menor) {
             menor = vet[i];
             indexMenor = i;
@@ -36,7 +37,7 @@ void TrocaSeAcharMenor(int *vet, int tam, int *paraTrocar) {
 }
 
 void OrdeneCrescente(int *vet, int tam) {
-    for (int i = 0; i < tam - 1; i++) {
+    for (int i = 0; i < tam; i++) {
         int indexMenor = i;
         TrocaSeAcharMenor(vet, tam, &indexMenor);
     }
