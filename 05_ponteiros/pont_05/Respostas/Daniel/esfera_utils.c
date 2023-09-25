0380aeafc439311f8be2479267edb7fb78bfc134
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-24 21:22:29 -0300

    feat: Adicionar implementação completa do pont_05

---
+++ 05_ponteiros/pont_05/Respostas/Daniel/esfera_utils.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esfera_utils.h"
+#include <math.h>
+
+void CalculaVolume (float R, float *volume) {
+    *volume = 4.0 * PI * pow(R, 3) / 3.0;
+}
+
+void CalculaArea (float R, float *area) {
+    *area = 4.0 * PI * pow(R, 2);
+}
