c72bedc7cbfe12a7eca191b04c25ad67a0bed0b7
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-25 23:07:48 -0300

    Adicionar implementação completa de main.c e concluir o TAD_03

---
+++ 04_TAD_simples/TAD_03/Respostas/Daniel/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "locadora.h"
+
+int main(void) {
+    char operacao[20];
+    tLocadora locadora = criarLocadora();
+
+    while (1) {
+        scanf("%s", operacao);
+         
+        if (!strcmp(operacao, "Cadastrar")) {
+            locadora = lerCadastroLocadora(locadora);
+            locadora = ordenarFilmesLocadora(locadora);
+            scanf("%*c");
+        } else if (!strcmp(operacao, "Alugar")) {
+            locadora = lerAluguelLocadora(locadora);
+            scanf("%*c");
+        } else if (!strcmp(operacao, "Devolver")) {
+            locadora = lerDevolucaoLocadora(locadora);
+            scanf("%*c");
+        } else if (!strcmp(operacao, "Estoque")) {
+            consultarEstoqueLocadora(locadora);
+            scanf("%*[^#]#");
+        }else {
+            break;
+        }
+
+        *operacao = '\0';
+    }
+    
+    consultarLucroLocadora(locadora);
+
+    return 0;
+}
