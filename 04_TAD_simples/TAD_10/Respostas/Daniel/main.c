8e109cb79dee8eb34b481e54f1ae0a79de8c5479
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-09-27 12:00:47 -0300

    Adicionar resolução completa main.c

---
+++ 04_TAD_simples/TAD_10/Respostas/Daniel/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "biblioteca.h"
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+
+    printf("Lista de operacoes:\n");
+    printf("1 - Cadastrar livros.\n");
+    printf("2 - Remover livros.\n");
+    printf("3 - Listar todos os livros cadastrados.\n");
+    printf("\n");
+
+    tLivros livro;
+    tBiblioteca biblioteca;
+    biblioteca = inicializarBiblioteca();
+
+    int operacao;
+    char titulo[100];
+    
+    while (N > 0) {
+        
+        scanf("%d", &operacao);
+
+        switch (operacao) {
+            case 1:
+                livro = lerLivro();
+                biblioteca = adicionarLivroNaBiblioteca(biblioteca, livro);
+                break;
+            case 2:    
+                scanf("%*c");
+                scanf("%99[^\n]\n", titulo);
+                biblioteca = removerLivroDaBiblioteca(biblioteca, titulo);
+                
+                break;
+            case 3:
+                listarLivrosDaBiblioteca(biblioteca);
+                break;
+            default:
+                printf("Operacao invalida!\n");
+                return 1;
+        }
+    
+        N--;
+    } 
+    return 0;
+}
