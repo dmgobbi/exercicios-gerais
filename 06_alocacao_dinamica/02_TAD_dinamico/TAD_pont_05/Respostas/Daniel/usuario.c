59bff249005630de9acdd00f70324ab8e10568e4
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-10-17 14:15:38 -0300

    Adicionar verificacao de alocacao de memoria para a variavel 'user'

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/usuario.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/usuario.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "usuario.h"
 
 tUsuario *CriaUsuario() {
     tUsuario* user = (tUsuario*) malloc(sizeof(tUsuario));
+
+    if (user == NULL) {
+        printf("Erro na alocacao de memoria.\n");
+        exit(1);
+    }
+
     user->nome[0] = '\0';
     user->cpf[0] = '\0';
     return user;
