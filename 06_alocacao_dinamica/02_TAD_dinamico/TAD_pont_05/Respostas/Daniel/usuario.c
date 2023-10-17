4decc68d09f2c5931215077d80a27bef6aa678d9
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-10-17 14:02:36 -0300

    Adicionar resolucao completa usuario.c

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/usuario.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/usuario.c
@@ -1,15 +1,23 @@
-// #include "usuario.h"
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include "usuario.h"
 
-// tUsuario CriaUsuario(char nome[20], char cpf[15]) {
-//     tUsuario usuario;
-//     strcpy(usuario.nome, nome);
-//     strcpy(usuario.cpf, cpf);
-//     return usuario;
-// }
+tUsuario *CriaUsuario() {
+    tUsuario* user = (tUsuario*) malloc(sizeof(tUsuario));
+    user->nome[0] = '\0';
+    user->cpf[0] = '\0';
+    return user;
+}
 
-// void ImprimeUsuario(tUsuario user) {
-//     printf("Nome: %s\n", user.nome);
-//     printf("CPF: %s\n", user.cpf);
-// }
+void DestroiUsuario(tUsuario *user) {
+    free(user);
+}
+
+void LeUsuario(tUsuario *user) {
+    scanf("%s %s", user->nome, user->cpf);
+}
+
+void ImprimeUsuario(tUsuario *user) {
+    printf("Nome: %s\n", user->nome);
+    printf("CPF: %s\n", user->cpf);
+}
