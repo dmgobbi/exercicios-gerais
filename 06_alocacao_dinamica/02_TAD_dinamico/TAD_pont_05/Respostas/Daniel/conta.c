632732bddc913035ab283dfe3302aa8cf644309c
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-10-19 13:52:05 -0300

    Adicionar resolucao completa conta.c

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/conta.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_05/Respostas/Daniel/conta.c
@@ -1,30 +1,60 @@
-// #include "conta.h"
-// #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "conta.h"
 
-// tConta CriaConta(int numero, tUsuario user) {
-//     tConta conta;
-//     conta.numero = numero;
-//     conta.user = user;
-//     conta.saldo = 0;
-//     return conta;
-// }
+tConta *CriaConta() {
+    tConta* conta = (tConta*) malloc(sizeof(tUsuario));
 
-// void ImprimeConta(tConta conta) {
-//     printf("Conta: %d\n", conta.numero);
-//     printf("Saldo: R$ %.2f\n", conta.saldo);
-//     ImprimeUsuario(conta.user);
-// }
+    if (conta == NULL) {
+        printf("Falha na alocacao de memoria\n");
+        exit(1);
+    } 
 
-// int VerificaConta(tConta conta, int numero) {
-//     return conta.numero == numero;
-// }
+    conta->numero = NULL;
+    conta->user = CriaUsuario();
+    conta->saldo = 0;
+    return conta;
+}
 
-// tConta SaqueConta(tConta conta, float valor) {
-//     conta.saldo -= valor;
-//     return conta;
-// }
+void DestroiConta(tConta *conta) {
+    free(conta->user);
+    free(conta);
+}
 
-// tConta DepositoConta(tConta conta, float valor) {
-//     conta.saldo += valor;
-//     return conta;
-// }
+void LeConta(tConta *conta) {
+    LeUsuario(conta->user);
+    if (scanf("%d\n", &conta->numero) != 1) {
+        printf("Erro na leitura do numero da conta.\n");
+    }
+}
+
+void ImprimeConta(tConta *conta) {
+    printf("Conta: %d\n", conta->numero);
+    printf("Saldo: R$ %.2f\n", conta->saldo);
+    ImprimeUsuario(conta->user);
+}
+
+int VerificaConta(tConta *conta, int numero) {
+    return conta->numero == numero;
+}
+
+void SaqueConta(tConta *conta, float valor) {
+    if (valor <= conta->saldo) {
+        conta->saldo -= valor;
+    } else {
+        printf("Saldo insuficiente.\n");
+    }
+}
+
+void DepositoConta(tConta *conta, float valor) {
+    conta->saldo += valor;
+}
+
+void TransferenciaConta(tConta *destino, tConta *origem, float valor) {
+    if (origem->saldo >= valor) {
+        SaqueConta(origem, valor);
+        DepositoConta(destino, valor);
+    } else {
+        printf("Saldo insuficiente.\n");
+    }
+}
