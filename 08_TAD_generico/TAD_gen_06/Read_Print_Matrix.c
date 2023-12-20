850258a65f3db30d13cf702d009aff0ba475cc14
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-12-20 03:01:48 -0300

    Adicionar continuacao da resolucao do main.c

--- 08_TAD_generico/TAD_gen_06/Read_Print_Matrix.c
+++ 08_TAD_generico/TAD_gen_06/Read_Print_Matrix.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 // Callback function types for reading and printing elements
 typedef void (*ReadElementFunc)(void *);
@@ -25,70 +24,36 @@ void printChar(void *elem);
 void readComplex(void *elem);
 void printComplex(void *elem);
 
-void TabelaImprimeMatriz(tMatrizGenerica *mat, int dataType);
-
 int main() {
     int rows, cols, dataType;
     tMatrizGenerica *mat;
 
-    printf("Digite o número de linhas, colunas e o tipo da matriz (0 – inteiro, 1 – float, 2 – double, 3 – char, 4 – número complexo): \n");
-
     // Reading the matrix dimensions and data type
-    if (scanf("%d %d %d", &rows, &cols, &dataType) != 3) {
-        printf("Error reading matrix dimensions and data type\n");
-        return 1;
-    }
+    scanf("%d %d %d", &rows, &cols, &dataType);
 
     switch (dataType) {
         case 0: // Integer
             mat = readMatrix(rows, cols, sizeof(int), readInt);
+            printMatrix(mat, printInt, ImprimirMatrizGenerica);
             break;
         case 1: // Float
             mat = readMatrix(rows, cols, sizeof(float), readFloat);
+            printMatrix(mat, printFloat, ImprimirMatrizGenerica);
             break;
         case 2: // Double
             mat = readMatrix(rows, cols, sizeof(double), readDouble);
+            printMatrix(mat, printDouble, ImprimirMatrizGenerica);
             break;
         case 3: // Char
             mat = readMatrix(rows, cols, sizeof(char), readChar);
+            printMatrix(mat, printChar, ImprimirMatrizGenerica);
             break;
         case 4: // Complex
             mat = readMatrix(rows, cols, RetornaNumBytesComplexo(), readComplex);
+            printMatrix(mat, printComplex, ImprimirMatrizGenerica);
             break;
         default:
             printf("Invalid data type\\n");
-            return 1;
-    }
-
-    printf("Digite a operação desejada\n");
-    printf("\t1 - Apenas imprimir a matriz\n");
-	printf("\t2 - Converter para o tipo complexo e imprimir");
-	printf("\t3 - Calcular e imprimir a multiplicacao da matriz pela sua transposta.");
-
-    int op;
-
-    while (true) {
-        if (scanf("%d", &op) != 1) {
-            printf("Error reading operation\n");
-            DestroiMatrizGenerica(mat);
-            return 1;
-        }
-
-        switch (op) {
-            case 1:
-                TabelaImprimeMatriz(mat, dataType);
-                break;
-            case 2:
-                // Convert to complex
-                break;
-            case 3:
-                // Multiply by its transpose
-                break;
-            default:
-                printf("Invalid operation\\n");
-                DestroiMatrizGenerica(mat);
-                return 1;
-        }
     }
 
     DestroiMatrizGenerica(mat);
@@ -138,7 +103,7 @@ void readComplex(void *elem) {
 
 void printComplex(void *elem) {
     tNumComplexo *cplx = (tNumComplexo *)elem;
-    printf("%.2f + i%.2f", PegaParteRealComplexo(cplx), PegaParteImagComplexo(cplx));
+    printf("(%.2f,%.2f)", PegaParteRealComplexo(cplx), PegaParteImagComplexo(cplx));
 }
 
 tMatrizGenerica* readMatrix(int rows, int cols, int elemSize, ReadElementFunc readElem) {
@@ -159,26 +124,4 @@ tMatrizGenerica* readMatrix(int rows, int cols, int elemSize, ReadElementFunc re
 
 void printMatrix(tMatrizGenerica *mat, PrintElementFunc printElem, PrintMatrixFunc printMat) {
     printMat(mat, printElem);
-}
-
-void TabelaImprimeMatriz(tMatrizGenerica *mat, int dataType) {
-    switch (dataType) {
-        case 0: // Integer
-            printMatrix(mat, printInt, ImprimirMatrizGenerica);
-            break;
-        case 1: // Float
-            printMatrix(mat, printFloat, ImprimirMatrizGenerica);
-            break;
-        case 2: // Double
-            printMatrix(mat, printDouble, ImprimirMatrizGenerica);
-            break;
-        case 3: // Char
-            printMatrix(mat, printChar, ImprimirMatrizGenerica);
-            break;
-        case 4: // Complex
-            printMatrix(mat, printComplex, ImprimirMatrizGenerica);
-            break;
-        default:
-            printf("Invalid data type\\n");
-    }
 }
