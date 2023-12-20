d064f09445a0f306e17c2246df04fac4fef06c19
dmgobbi <daniel.gobbi@edu.ufes.br>
2023-12-20 01:18:07 -0300

    Adicionar resolucao de matrizgenerica.c e numcomplexo.c

---
+++ 08_TAD_generico/TAD_gen_06/Respostas/Daniel/matrizgenerica.c
@@ -0,0 +1,147 @@
+#include "matrizgenerica.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct matrizgenerica {
+    int l;
+    int c;
+    int numByteElem;
+    void *elementos;
+};
+
+tMatrizGenerica * CriaMatrizGenerica(int linhas, int colunas, int numByteElem) {
+    tMatrizGenerica * m = malloc(sizeof(tMatrizGenerica));
+    if (!m) {
+        printf("Erro ao alocar memoria para matriz\n");
+        return NULL;
+    }
+    m->l = linhas;
+    m->c = colunas;
+    m->numByteElem = numByteElem;
+    m->elementos = calloc(m->l * m->c, numByteElem);
+    if (!m->elementos) {
+        printf("Erro ao alocar memoria para o vetor de elementos da matriz\n");
+        return NULL;
+    }
+    return m;
+}
+
+void DestroiMatrizGenerica(tMatrizGenerica * m) {
+    if (!m) {
+        printf("Erro ao destruir matriz\n");
+        return;
+    }
+    free(m->elementos);
+    free(m);
+}
+
+int ObtemNumeroLinhasMatrizGenerica(tMatrizGenerica * m) {
+    if (m) {
+        return m->l;
+    } else {
+        printf("Erro ao obter numero de linhas da matriz\n");
+        return -1;  
+    }
+}
+
+int ObtemNumeroColunasMatrizGenerica(tMatrizGenerica * m) {
+    if (m) {
+        return m->c;
+    } else {
+        printf("Erro ao obter numero de colunas da matriz\n");
+        return -1;
+    }
+}
+
+void * ObtemElementoMatrizGenerica(tMatrizGenerica * m, int l, int c) {
+    if (m) {
+        return m->elementos + (l * (m->c * m->numByteElem) + (c * m->numByteElem));
+    } else {
+        printf("Erro ao obter elemento da matriz\n");
+        return NULL; 
+    }
+}
+
+void AtribuiElementoMatrizGenerica(tMatrizGenerica * m, int l, int c, void* elem) {
+    if (m) {
+        void * elemMat = ObtemElementoMatrizGenerica(m, l, c);
+        memcpy(elemMat, elem, m->numByteElem);
+    } else {
+        printf("Erro ao atribuir elemento na matriz\n");
+        return;
+    }
+}
+
+void ImprimirMatrizGenerica(tMatrizGenerica * m, void (imprime_elemento(void*))) {
+    if (m) {
+        for (int i = 0; i < m->l; i++) {
+            for (int j = 0; j < m->c; j++) {
+                imprime_elemento(ObtemElementoMatrizGenerica(m, i, j));
+                printf(" ");
+            }
+            printf("\n");
+        }
+    } else {
+        printf("Erro ao imprimir matriz\n");
+        return;
+    }
+}
+
+tMatrizGenerica *MatrizTransposta(tMatrizGenerica *m) {
+    if (m) {
+        tMatrizGenerica *mT = CriaMatrizGenerica(m->c, m->l, m->numByteElem);
+        for (int i = 0; i < m->l; i++) {
+            for (int j = 0; j < m->c; j++) {
+                AtribuiElementoMatrizGenerica(mT, j, i, ObtemElementoMatrizGenerica(m, i, j));
+            }
+        }
+        return mT;
+    } else {
+        printf("Erro ao transpor matriz\n");
+        return NULL;
+    }
+}
+
+tMatrizGenerica *MultiplicaMatrizes(tMatrizGenerica *mat1, tMatrizGenerica *mat2, int numByteTarget, void* (*multi_elem)(void*, void*), void* (*soma_elem)(void*,void*)) {
+    if (mat1 && mat2) {
+        if (mat1->c == mat2->l) {
+            tMatrizGenerica *m = CriaMatrizGenerica(mat1->l, mat2->c, numByteTarget);
+            for (int i = 0; i < mat1->l; i++) {
+                for (int j = 0; j < mat2->c; j++) {
+                    void *elem = ObtemElementoMatrizGenerica(m, i, j);
+                    for (int k = 0; k < mat1->c; k++) {
+                        void *elem1 = ObtemElementoMatrizGenerica(mat1, i, k);
+                        void *elem2 = ObtemElementoMatrizGenerica(mat2, k, j);
+                        elem = soma_elem(elem, multi_elem(elem1, elem2));
+                    }
+                    AtribuiElementoMatrizGenerica(m, i, j, elem);
+                }
+            }
+            return m;
+        } else {
+            printf("Erro ao multiplicar matrizes.\nNumero de colunas da primeira matriz diferente do numero de linhas da segunda matriz\n");
+            return NULL;
+        }
+    } else {
+        printf("Erro ao multiplicar matrizes.\nUma das matrizes nao existe\n");
+        return NULL;
+    }
+}
+
+tMatrizGenerica *ConverteTipoMatriz(tMatrizGenerica *mat2, int novoNumByteElem, void* (*converte_elem)(void*)) {
+    if (mat2) {
+        tMatrizGenerica *m = CriaMatrizGenerica(mat2->l, mat2->c, novoNumByteElem);
+        for (int i = 0; i < mat2->l; i++) {
+            for (int j = 0; j < mat2->c; j++) {
+                void *elem = ObtemElementoMatrizGenerica(mat2, i, j);
+                elem = converte_elem(elem);
+                AtribuiElementoMatrizGenerica(m, i, j, elem);
+            }
+        }
+        return m;
+    } else {
+        printf("Erro ao converter matriz.\n");
+        return NULL;
+    }
+}
---
+++ 08_TAD_generico/TAD_gen_06/Respostas/Daniel/numcomplexo.c
@@ -0,0 +1,62 @@
+#include "numcomplexo.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct complexo {
+    float real;
+    float imag;
+};
+
+tNumComplexo* CriaNumComplexo(float real, float imag) {
+    tNumComplexo *cplx = (tNumComplexo*) malloc(sizeof(tNumComplexo));
+    if (cplx == NULL) {
+        printf("Erro ao alocar memória para o número complexo\n");
+        exit(1);
+    }
+    cplx->real = real;
+    cplx->imag = imag;
+    return cplx;
+}
+
+float PegaParteImagComplexo(tNumComplexo *cplx) {
+    return cplx->imag;
+}
+
+float PegaParteRealComplexo(tNumComplexo *cplx) {
+    return cplx->real;
+}
+
+void ImprimeNumeroComplexo(tNumComplexo *cplx) {
+    printf("%.2f + i%.2f", cplx->real, cplx->imag);
+}
+
+void DestroiNumeroComplexo(tNumComplexo *cplx) {
+    free(cplx);
+}
+
+int RetornaNumBytesComplexo() {
+    return sizeof(struct complexo);
+}
+
+tNumComplexo* SomaComplexos(tNumComplexo* n1, tNumComplexo *n2) {
+    tNumComplexo *cplx = (tNumComplexo*) malloc(sizeof(tNumComplexo));
+    if (cplx == NULL) {
+        printf("Erro ao alocar memória para o número complexo\n");
+        exit(1);
+    }
+    cplx->real = n1->real + n2->real;
+    cplx->imag = n1->imag + n2->imag;
+    return cplx;
+}
+
+tNumComplexo* MultComplexos(tNumComplexo* n1, tNumComplexo* n2) {
+    tNumComplexo *cplx = (tNumComplexo*) malloc(sizeof(tNumComplexo));
+    if (cplx == NULL) {
+        printf("Erro ao alocar memória para o número complexo\n");
+        exit(1);
+    }
+    cplx->real = (n1->real * n2->real) - (n1->imag * n2->imag);
+    cplx->imag = (n1->real * n2->imag) + (n1->imag * n2->real);
+    return cplx;
+}
